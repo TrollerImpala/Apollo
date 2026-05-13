429f16542cb402488ccb26736bd878074e7b8146
TrollerImpala <gamerlautaroandrada@gmail.com>
2026-05-13 16:54:47 -0300

    frontend test

---
+++ frontend/script.js
@@ -0,0 +1,10 @@
+const visualizer = document.getElementById("visualizer");
+const Vctx = visualizer.getContext("2d");
+Vctx.fillStyle = "#3C095C";
+Vctx.fillRect(0, 0, 500, 500);
+
+//Vctx.beginPath();
+//Vctx.moveTo(20, 20);
+//Vctx.lineTo(20, 100);
+//Vctx.lineTo(70, 100);
+//Vctx.stroke();
